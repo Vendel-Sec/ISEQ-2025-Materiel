5d71511c75808ec30f2bc30574b9ea9aa39e08c2
Colin Stephenne <colin.stephenne@vendel.ca>
2025-02-05 17:20:44 +0000

    Add firmware for hardware exercices

---
+++ Materiel/flash_firmware.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Flash firmware
+espflash flash --flash-size 8mb firmware.bin
+
+# Flash NVS
+source ~/esp/esp-idf/export.sh
+python ~/esp/esp-idf/components/partition_table/parttool.py -b 921600 write_partition --partition-name=nvs --input=nvs.bin
